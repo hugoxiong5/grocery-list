a46fe6b0cbbe8c3c20c528b06426351c6691ad73
Hugo Xiong <hugoxiong@gmail.com>
2020-09-09 11:51:13 -0700

    add list element

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,21 @@
 "use strict";
+// import List from "./components/list.js";
 
+const ListItem = (props) => <li>{props.item}</li>;
 
+const List = (props) => (
+  <ul>
+    {props.list.map((item, index) => {
+      return <ListItem key={index} item={item} />;
+    })}
+  </ul>
+);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [],
+      list: ["hi", "world"],
     };
     this._handleChange = this._handleChange.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
@@ -14,15 +23,33 @@ class App extends React.Component {
 
   _handleChange(e) {
     console.log("Event happened");
+    return setState({
+
+    })
+
+    }
   }
 
   _handleSubmit() {
+    e.preventDefault();
     console.log("You submitted the form!");
   }
 
   render() {
-    return <div>Test</div>;
+    return (
+      <div>
+        <List list={this.state.list} />
+        <form onSubmit={this._handleSubmit}>Test</form>
+        <label>
+          Cart:{' '}
+          <input type="text"
+          name="list"
+          value={this.state.list}
+          onChange={this._handleChange} />
+        </label>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
